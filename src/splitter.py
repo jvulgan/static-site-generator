4584916bed30390f6c564756b6938753c06d1043
Jakub Vulgan <jvulgan@redhat.com>
2025-09-28 19:23:45 +0200

    Add func for extracting md links and imgs

--- src/splitter.py
+++ src/splitter.py
@@ -1,11 +1,18 @@
+import re
 from textnode import TextType, TextNode
+
+
+IMAGE_REGEX = re.compile(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)")
+LINK_REGEX = re.compile(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)")
 
 
 class InvalidMarkdownError(Exception):
     pass
 
 
-def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+) -> list[TextNode]:
     """
     Split provided TextNodes based on provided delimiter and text_type.
 
@@ -21,7 +28,7 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
             continue
         splitted = node.text.split(delimiter)
         if len(splitted) % 2 == 0:
-            raise InvalidMarkdownError('no closing delimiter detected')
+            raise InvalidMarkdownError("no closing delimiter detected")
         for idx, text in enumerate(splitted):
             if idx % 2 != 0:
                 new_node = TextNode(text, text_type)
@@ -33,3 +40,16 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
             new_nodes.append(new_node)
     return new_nodes
 
+
+def extract_markdown_images(text: str) -> list[tuple]:
+    """
+    Extract markdown images from raw text and return a list of tuples (alt_text, url).
+    """
+    return re.findall(IMAGE_REGEX, text)
+
+
+def extract_markdown_links(text: str) -> list[tuple]:
+    """
+    Extract markdown links from raw text and return a list of tuples (anchor_text, url).
+    """
+    return re.findall(LINK_REGEX, text)
--- src/test_splitter.py
+++ src/test_splitter.py
@@ -1,10 +1,16 @@
 import unittest
-from splitter import InvalidMarkdownError, split_nodes_delimiter
+from splitter import (
+    InvalidMarkdownError,
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+)
 from textnode import TextType, TextNode
+
 
 class TestSplitter(unittest.TestCase):
     def test_split_nodes_delimiter_empty_nodes(self):
-        self.assertEqual(split_nodes_delimiter([], '*', TextType.BOLD), [])
+        self.assertEqual(split_nodes_delimiter([], "*", TextType.BOLD), [])
 
     def test_split_nodes_delimiter_only_nontext(self):
         nodes = [
@@ -12,11 +18,13 @@ class TestSplitter(unittest.TestCase):
             TextNode("this is italic", TextType.ITALIC),
             TextNode("this is code", TextType.CODE),
         ]
-        self.assertEqual(split_nodes_delimiter(nodes, '*', TextType.BOLD), nodes)
+        self.assertEqual(split_nodes_delimiter(nodes, "*", TextType.BOLD), nodes)
 
     def test_split_nodes_delimiter_no_closing_delimiter(self):
         with self.assertRaises(InvalidMarkdownError):
-            split_nodes_delimiter([TextNode("bold only starts *here", TextType.TEXT)], '*', TextType.BOLD)
+            split_nodes_delimiter(
+                [TextNode("bold only starts *here", TextType.TEXT)], "*", TextType.BOLD
+            )
 
     def test_split_nodes_delimiter_code(self):
         node = TextNode("text `code` text", TextType.TEXT)
@@ -25,7 +33,7 @@ class TestSplitter(unittest.TestCase):
             TextNode("code", TextType.CODE),
             TextNode(" text", TextType.TEXT),
         ]
-        self.assertEqual(split_nodes_delimiter([node], '`', TextType.CODE), expected)
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE), expected)
 
     def test_split_nodes_delimiter_bold(self):
         node = TextNode("text **bold** text", TextType.TEXT)
@@ -34,7 +42,7 @@ class TestSplitter(unittest.TestCase):
             TextNode("bold", TextType.CODE),
             TextNode(" text", TextType.TEXT),
         ]
-        self.assertEqual(split_nodes_delimiter([node], '**', TextType.CODE), expected)
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.CODE), expected)
 
     def test_split_nodes_delimiter_italic(self):
         node = TextNode("text *italic* text", TextType.TEXT)
@@ -43,7 +51,7 @@ class TestSplitter(unittest.TestCase):
             TextNode("italic", TextType.CODE),
             TextNode(" text", TextType.TEXT),
         ]
-        self.assertEqual(split_nodes_delimiter([node], '*', TextType.CODE), expected)
+        self.assertEqual(split_nodes_delimiter([node], "*", TextType.CODE), expected)
 
     def test_split_nodes_delimiter_multiple_match(self):
         node = TextNode("text `code` `another code` text", TextType.TEXT)
@@ -54,8 +62,7 @@ class TestSplitter(unittest.TestCase):
             TextNode("another code", TextType.CODE),
             TextNode(" text", TextType.TEXT),
         ]
-        self.assertEqual(split_nodes_delimiter([node], '`', TextType.CODE), expected)
-
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE), expected)
 
     def test_split_nodes_delimiter_multiple_with_empty_strings(self):
         node = TextNode("`code` text `another`", TextType.TEXT)
@@ -64,5 +71,50 @@ class TestSplitter(unittest.TestCase):
             TextNode(" text ", TextType.TEXT),
             TextNode("another", TextType.CODE),
         ]
-        self.assertEqual(split_nodes_delimiter([node], '`', TextType.CODE), expected)
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE), expected)
 
+    def test_extract_markdown_images_single(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_nothing(self):
+        matches = extract_markdown_images("no links here")
+        self.assertEqual([], matches)
+
+    def test_extract_markdown_images_multiple(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links_single(self):
+        matches = extract_markdown_links(
+            "This is text with a [single link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("single link", "https://i.imgur.com/zjjcJKZ.png")], matches
+        )
+
+    def test_extract_markdown_links_nothing(self):
+        matches = extract_markdown_links("no images here")
+        self.assertEqual([], matches)
+
+    def test_extract_markdown_links_multiple(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        expected = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev"),
+        ]
+
+        self.assertListEqual(
+            expected,
+            matches,
+        )
