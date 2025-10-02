64325f608850adb59d48ebf48b33631573c88322
Jakub Vulgan <jvulgan@redhat.com>
2025-10-02 11:19:45 +0200

    Add generating to docs folder for GH pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from converter import markdown_to_html_node
 from splitter import extract_title
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str):
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         markdown = f.read()
@@ -12,19 +12,26 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
     html_node = markdown_to_html_node(markdown)
     title = extract_title(markdown)
     html = html_node.to_html()
-    page = html_template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    page = (
+        html_template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
+    )
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    with open(dest_path, 'w') as f:
+    with open(dest_path, "w") as f:
         f.write(page)
 
 
-def generate_page_recursive(dir_path_content: str, template_path: str, dest_dir_path: str):
+def generate_page_recursive(
+    dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str
+):
     for item in os.listdir(dir_path_content):
         item_path = os.path.join(dir_path_content, item)
         if os.path.isfile(item_path):
             item = item.replace(".md", ".html")
             dest_path = os.path.join(dest_dir_path, item)
-            generate_page(item_path, template_path, dest_path)
+            generate_page(item_path, template_path, dest_path, basepath)
         else:
             dest_path = os.path.join(dest_dir_path, item)
-            generate_page_recursive(item_path, template_path, dest_path)
+            generate_page_recursive(item_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
+import sys
 from textnode import TextType, TextNode
 from copyfiles import copy_files
 from generate_page import generate_page_recursive
 
 
 def main():
-    copy_files('static', 'public')
-    generate_page_recursive("content", "template.html", "public")
-
+    args = sys.argv
+    if len(args) != 2:
+        basepath = "/"
+    else:
+        basepath = args[1]
+    copy_files("static", "docs")
+    generate_page_recursive("content", "template.html", "docs", basepath)
 
 
 if __name__ == "__main__":
