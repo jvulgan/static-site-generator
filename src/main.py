8247a09985212a3273d1492f6b3311ab786331e7
Jakub Vulgan <jvulgan@redhat.com>
2025-10-02 10:49:12 +0200

    Add more subpages to the blog

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ from generate_page import generate_page
 def main():
     copy_files('static', 'public')
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
+    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
+    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
+    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
 
 
 
